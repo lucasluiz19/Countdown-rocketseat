7612c146decf8b1f7025cc90ede7beacc599ea3f
lucasluiz19 <lucas844@outlook.com.br>
2022-08-10 21:46:36 -0300

    finsh-commit

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ const minuteselements=document.getElementById('minutes');
 const secondselements=document.getElementById('seconds');
  
 //data desejada de projeto
-const dataprog=" 16 aug 2022";
+const dataprog="19 aug 2022 ";//dd/mm/aa
 
 //inicia a função
 function countdown() {
-    const dateNewProject= new Date(dataprog);
-    const currentDate= new Date();
+    const dateNewProject= new Date(dataprog);//atribui a const dataprog, que é a data desejada em new date()
+    const currentDate= new Date(); //data atua !
+    
      
     const totalSeconds=(dateNewProject - currentDate) / 1000;
     const days= Math.floor(totalSeconds / 3600 / 24);
@@ -19,15 +20,15 @@ function countdown() {
     const seconds=Math.floor(totalSeconds) % 60;
 
     dayselements.innerHTML= days;
-    hourselements.innerHTML=formatTime(hours)
+    hourselements.innerHTML=formatTime(hours);//passa a função de add um zero 
     minuteselements.innerHTML=formatTime(minutes) ;
     secondselements.innerHTML=formatTime(seconds) ;
 
 
 }
-function formatTime(time) {
-    return time <10 ? `0 ${time}`: time;
+function formatTime(time) {//função para adicionar um zero quando o numero da hora, minuto e segundo estiver menor que 10 
+    return time <10 ? `0${time}`: time;
     
 }
-countdown();
-setInterval(countdown, 1000);
+countdown();//executa a função pricipal 
+setInterval(countdown, 1000);//passa um intervalo para a a função de 1000 ms... Ela executa uma função de retorno após determinado tempo.
